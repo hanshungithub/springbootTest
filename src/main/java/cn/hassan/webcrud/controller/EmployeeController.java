3908f04602e7cf2f08907aa3d558597cd25d9c3a
hanshungithub <hshanshun@163.com>
2018-04-07 16:03:58 +0800

    add

--- src/main/java/cn/hassan/webcrud/controller/EmployeeController.java
+++ src/main/java/cn/hassan/webcrud/controller/EmployeeController.java
@@ -4,14 +4,19 @@ import cn.hassan.webcrud.dao.DepartmentDao;
 import cn.hassan.webcrud.dao.EmployeeDao;
 import cn.hassan.webcrud.entities.Department;
 import cn.hassan.webcrud.entities.Employee;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 import java.util.Collection;
 
 @Controller
+@Slf4j
 public class EmployeeController {
 
     @Autowired
@@ -32,5 +37,27 @@ public class EmployeeController {
         Collection<Department> departments = departmentDao.getDepartments();
         model.addAttribute("depts", departments);
         return "emps/add";
+    }
+
+    @PostMapping("/emp")
+    public String addEmp(Employee employee) {
+        log.info("info --> "+employee.toString());
+        employeeDao.save(employee);
+        return "redirect:/emps";
+    }
+
+    @GetMapping("/emp/{id}")
+    public String toEditPage(@PathVariable("id") Integer id, Model model) {
+        Employee employee = employeeDao.get(id);
+        model.addAttribute("emp", employee);
+        Collection<Department> departments = departmentDao.getDepartments();
+        model.addAttribute("depts", departments);
+        return "emps/add";
+    }
+
+    @PutMapping("/emp")
+    public String updateEmp(Employee employee) {
+        employeeDao.save(employee);
+        return "redirect:/emps";
     }
 }
