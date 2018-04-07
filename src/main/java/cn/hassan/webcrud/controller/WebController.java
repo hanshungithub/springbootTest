bf6fb4aa9a7e9a38423ffaf5649801d2047f6784
hanshungithub <hshanshun@163.com>
2018-04-07 20:59:18 +0800

    add

--- src/main/java/cn/hassan/webcrud/controller/WebController.java
+++ src/main/java/cn/hassan/webcrud/controller/WebController.java
@@ -1,8 +1,10 @@
 package cn.hassan.webcrud.controller;
 
+import cn.hassan.webcrud.exception.UserNotExistException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Arrays;
 import java.util.Map;
@@ -15,5 +17,14 @@ public class WebController {
         map.put("hello", "<h1>hassan</h1>");
         map.put("users",Arrays.asList("sherry","tom","jack"));
         return "success";
+    }
+
+    @ResponseBody
+    @RequestMapping("/hello")
+    public String hello(@RequestParam("user") String user) {
+        if (user.equals("aaa")) {
+            throw new UserNotExistException();
+        }
+        return "hello";
     }
 }
